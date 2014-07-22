4fa452be682d97b99181c3d53455129e5fe9a1f4
morganherlocker <morgan.herlocker@gmail.com>
2014-07-22 17:25:14 -0400

    stubbed tests

---
+++ test.js
@@ -0,0 +1,41 @@
+var test = require('tape');
+
+test('point', function(t){
+
+	t.end()
+})
+
+test('linestring', function(t){
+
+	t.end()
+})
+
+test('polygon', function(t){
+
+	t.end()
+})
+
+test('multipoint', function(t){
+
+	t.end()
+})
+
+test('multilinestring', function(t){
+
+	t.end()
+})
+
+test('multipolygon', function(t){
+
+	t.end()
+})
+
+test('feature', function(t){
+
+	t.end()
+})
+
+test('featurecollection', function(t){
+
+	t.end()
+})
