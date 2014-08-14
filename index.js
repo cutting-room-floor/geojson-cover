917d5825c7fbf8b052e101dd9a0246ec79e18627
Tom MacWright <tom@macwright.org>
2014-08-14 15:13:32 -0400

    Fix style problems

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var serialization = 'toToken';
 
 module.exports.bboxQueryIndexes = function(bbox, range, opts) {
     opts = setOptions(opts);
-    if(range === undefined) range = true;
+    if (range === undefined) range = true;
     var latLngRect = new s2.S2LatLngRect(
         new s2.S2LatLng(bbox[1], bbox[0]),
         new s2.S2LatLng(bbox[3], bbox[2]));
@@ -19,7 +19,7 @@ module.exports.bboxQueryIndexes = function(bbox, range, opts) {
     };
 
     return s2.getCover(latLngRect, cover_options).map(function(cell) {
-        if(range){
+        if (range) {
             return [
                 cell.id().rangeMin().toToken(),
                 cell.id().rangeMax().toToken()
@@ -91,8 +91,8 @@ function linestringIndex(geometry, opts) {
     var feature = {
         type:'Feature',
         geometry: geometry
-    }
-    geometry = buffer(feature, .00001, 'miles').features[0].geometry;
+    };
+    geometry = buffer(feature, 0.00001, 'miles').features[0].geometry;
     var rings = geometry.coordinates;
 
     var cover_options = {
@@ -102,8 +102,8 @@ function linestringIndex(geometry, opts) {
         type: 'polygon'
     };
 
-    var llRings = rings.map(function(ring){
-        return ring.map(function(c){
+    var llRings = rings.map(function(ring) {
+        return ring.map(function(c) {
             var latLng = (new s2.S2LatLng(c[1], c[0])).normalized();
             return latLng.toPoint();
         }).slice(1);
@@ -124,8 +124,8 @@ function polygonIndex(geometry, opts) {
         type: 'polygon'
     };
 
-    var llRings = rings.map(function(ring){
-        return ring.map(function(c){
+    var llRings = rings.map(function(ring) {
+        return ring.map(function(c) {
             var latLng = (new s2.S2LatLng(c[1], c[0])).normalized();
             return latLng.toPoint();
         }).slice(1);
@@ -146,8 +146,8 @@ function multipolygonIndex(geometry, opts) {
         type: 'multipolygon'
     };
 
-    var llRings = polygons.map(function(rings){
-        return rings.map(function(ring){
+    var llRings = polygons.map(function(rings) {
+        return rings.map(function(ring) {
             return ring.map(function(c){
                 var latLng = (new s2.S2LatLng(c[1], c[0])).normalized();
                 return latLng.toPoint();
@@ -167,7 +167,7 @@ function linestringGeoJSON(geometry, opts) {
         type:'Feature',
         geometry: geometry
     }
-    geometry = buffer(feature, .00001, 'miles').features[0].geometry;
+    geometry = buffer(feature, 0.00001, 'miles').features[0].geometry;
     var rings = geometry.coordinates;
 
     var cover_options = {
@@ -177,8 +177,8 @@ function linestringGeoJSON(geometry, opts) {
         type: 'polygon'
     };
 
-    var llRings = rings.map(function(ring){
-        return ring.map(function(c){
+    var llRings = rings.map(function(ring) {
+        return ring.map(function(c) {
             var latLng = (new s2.S2LatLng(c[1], c[0])).normalized();
             return latLng.toPoint();
         }).slice(1);
@@ -208,13 +208,13 @@ function polygonGeoJSON(geometry, opts) {
         type: 'polygon'
     };
 
-    var llRings = rings.map(function(ring){
-        return ring.map(function(c){
+    var llRings = rings.map(function(ring) {
+        return ring.map(function(c) {
             var latLng = (new s2.S2LatLng(c[1], c[0])).normalized();
             return latLng.toPoint();
         }).slice(1);
     });
-    var features = s2.getCover(llRings, cover_options).map(function(cell,i) {
+    var features = s2.getCover(llRings, cover_options).map(function(cell, i) {
         var geojson = cell.toGeoJSON();
         return {
             type: 'Feature',
@@ -239,15 +239,15 @@ function multipolygonGeoJSON(geometry, opts) {
         type: 'multipolygon'
     };
 
-    var llRings = polygons.map(function(rings){
-        return rings.map(function(ring){
+    var llRings = polygons.map(function(rings) {
+        return rings.map(function(ring) {
             return ring.map(function(c){
                 var latLng = (new s2.S2LatLng(c[1], c[0])).normalized();
                 return latLng.toPoint();
             }).slice(1);
         });
     });
-    var features = s2.getCover(llRings, cover_options).map(function(cell,i) {
+    var features = s2.getCover(llRings, cover_options).map(function(cell, i) {
         var geojson = cell.toGeoJSON();
         return {
             type: 'Feature',
@@ -262,54 +262,55 @@ function multipolygonGeoJSON(geometry, opts) {
     }
 }
 
-function setOptions(opts){
-    if(!opts){
+function setOptions(opts) {
+    if (!opts) {
         opts = {}
     }
 
     // the maximum number of S2 cells used for any query coverage.
     // - More = more complex queries
     // - Fewer = less accurate queries
-    if(!opts.max_query_cells){
+    if (opts.max_query_cells === undefined) {
         opts.max_query_cells = 100
     }
 
     // The largest size of a cell permissable in a query.
-    if(!opts.query_min_level){
+    if (opts.query_min_level === undefined) {
         opts.query_min_level = 1;
     }
 
     // The smallest size of a cell permissable in a query.
     // - This must be >= index_max_level
-    if(!opts.query_max_level){
+    if (opts.query_max_level === undefined) {
         opts.query_max_level = 8;
     }
 
     // the maximum number of S2 cells used for any index coverage.
     // - More = more accurate indexes
     // - Fewer = more compact queries
-    if(!opts.max_index_cells){
+    if (opts.max_index_cells === undefined) {
         opts.max_index_cells = 100;
     }
 
     // The largest size of a cell permissable in an index.
     // - This must be <= query_min_level
-    if(!opts.index_min_level){
+    if (opts.index_min_level === undefined) {
         opts.index_min_level = 8;
     }
 
     // The smallest size of a cell permissable in an index.
-    if(!opts.index_max_level){
+    if (opts.index_max_level === undefined) {
         opts.index_max_level = 12;
     }
 
     // The index level for point features only.
-    if(!opts.index_point_level){
+    if (opts.index_point_level === undefined) {
         opts.index_point_level = 15;
     }
 
-    if(!(opts.query_max_level >= opts.index_min_level)){
+    if (!(opts.query_max_level >= opts.index_min_level)) {
         throw new Error('query level and index level must correspond');
     }
+
     return opts;
 }
